39d7bf67b5fbf49c7cb67c8d110fdc250db14d7c
tatendabakozw <tatendabakozw@gmail.com>
2024-10-18 12:12:42 +0200

    fix: text color

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <div className="text-zinc-700">
+    <Component {...pageProps} />
+  </div>
 }
